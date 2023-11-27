e6c2ba4c9888eca310eb3d8d3a9288cedf388e9e
tauan.gramacho <tauangramacho.dev@gmail.com>
2023-11-27 15:50:51 -0300

    .

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,15 +1,20 @@
-import { useContext } from 'react';
-import { QuizContext } from '../context/quiz';
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
 
-import Quiz from '../img/quiz.svg';
-import './Welcome.css';
+import "./Welcome.css";
+
+import Quiz from "../img/quiz.svg";
 
 const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div id="welcome">
-      <h2 className="welcome-heading">Seja bem-vindo</h2>
+      <h2>Seja bem-vindo</h2>
       <p>Clique no botão abaixo para começar:</p>
-      <button>Iniciar</button>
+      <button onClick={() => dispatch({ type: "CHANGE_STAGE" })}>
+        Iniciar
+      </button>
       <img src={Quiz} alt="Início do Quiz" />
     </div>
   );
